8a694636b631ceae1a962089c57adedeec18f20a
LexLuter <gae_cbr@mail.ru>
2023-03-01 20:00:28 +0700

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] GetBinaryArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+    return array;
+}
+
+int size = 8;
+int[] resultArray = GetBinaryArray(size);
+int result = 0;
+Console.WriteLine($"Массив: [ {String.Join(", ", resultArray)} ]");
+for (int i = 0; i < size; i++)
+{
+    if (i != 0 & (i % 2 > 0)){ 
+        result = resultArray[i] + result;
+        //Console.WriteLine($"{resultArray[i]}");
+    }
+}
+
+Console.WriteLine($"Сумма элементов, находящихся на нечетных позициях: {result}");
